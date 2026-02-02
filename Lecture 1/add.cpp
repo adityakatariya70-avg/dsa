0be0f43f416a0b7cf0eb0dccd64a89cca9b133a1
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-02-02 21:03:37 +0530

    new commit

---
+++ Lecture 1/add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    cout<<"Enter A ";
+    cin>>a;
+    cout<<"Enter B ";
+    cin>>b;
+    int c=a+b;
+    cout<<"The sum is :";
+    cout<<c;
+    return 0;
+}
---
+++ Lecture 2/gcd_bruteforce.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cin>>a;
+cin>>b;
+while(b!=0){
+    int t=a%b;
+    a=b;
+    b=t;
+}
+cout<<a<<endl;
+}
