6a48c4b25b3a6be1cb46ae86a6aadae8380bdbe9
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-02-17 19:22:48 +0530

    vector string

--- Lecture 4/vector_string_2.cpp
+++ Lecture 4/vector_string_2.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
-#include<vector>
+#include<vector> //vector header file
 using namespace std;
 int main(){
-    vector<string> cars={"creta","baleno","alto","nano"};
-    cout<<cars[3];
+    vector<string> cars={"creta","baleno","alto","nano"}; //vector of string type and initialization
+    cout<<cars[0]<<endl; //access through index
+    cout<<cars.front()<<endl; //access through front function
+    cout<<cars.back()<<endl; //access through back function
+    cout<<cars.at(2)<<endl; //access through at function
+    cars[1]="honda";
+    cars[2]="hyundai";
+    cout<<cars.at(1)<<endl;
+    cout<<cars.at(2)<<endl;
+    cars.push_back("swift"); //inserting element at the end of vector
+    cout<<cars[4]<<endl;
+    cars.pop_back();
+    cout<<cars.back()<<endl; //after pop_back the last element is removed
+    for( string car : cars){ //traversing the vector using for each loop
+        cout<<car<<endl;
+    }
     
     return 0;
 }
