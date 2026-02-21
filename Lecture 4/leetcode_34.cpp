f724742c7112fb0b63bb597a46bf15004cbc3ff4
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-02-21 21:38:30 +0530

    leetcode 34

---
+++ Lecture 4/leetcode_34.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={4,6,6,7,8,5};
+    int target=6;
+    for(int i=0;i<5;i++){
+        arr[i]==target;
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
