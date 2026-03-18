0e289286fcd4667936eb7d8d78141b76d4bf1013
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-03-18 12:00:40 +0530

    sum

--- Lecture 1/reverse_digit.cpp
+++ Lecture 1/reverse_digit.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=123;
-    for(int i=0;i>=n;i--){
-        cout<<n;
+    int n;
+    cin>>n;
+    int rev=0;
+    while(n>0){
+       
+        rev=rev*10+n%10;
+        n=n/10;
     }
+    cout<<rev;
     return 0;
 }
